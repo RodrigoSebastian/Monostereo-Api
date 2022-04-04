cbd71562733ae4389473cf24131447eeeeb25b85
= <57414811+RodrigoSebastian@users.noreply.github.com>
2022-04-03 20:51:50 -0500

    Migrate DB: SQLite -> Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_03_235423) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -21,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_03_235423) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.integer "category_id", null: false
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
